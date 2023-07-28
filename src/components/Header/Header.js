57d98c1f179aaad565697d347acec527c43c1d87
Emimint <90863470+Emimint@users.noreply.github.com>
2023-07-28 14:29:47 -0400

    create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import styles from "./Header.module.css";
+
+
+const Header = ({logo}) => {
+  return (
+    <header
+      className={styles.header}
+    >
+      <img src={logo} alt="logo" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+};
+
+    export default Header;
